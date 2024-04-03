af263b139e9313a59addeeaac0e8ed71ce4eeef7
P-Florek <piotrektoja12@gmail.com>
2024-04-03 09:35:51 +0200

    Dodanie klasy Images oraz klasy ImageSource

---
+++ slider/Images.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace slider
+{
+    internal class Images
+    {
+        public string Path { get; set; }
+
+        public ImageSource Source { get; set; }
+    }
+}
